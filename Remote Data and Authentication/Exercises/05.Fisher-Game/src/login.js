0642622c4113586bb9770e48ede111f537853c40
Trifon Shterev <67158514+TrifShterev@users.noreply.github.com>
2022-02-11 13:25:42 +0200

    fisherGame Login functionality Ready and Works

--- Remote Data and Authentication/Exercises/05.Fisher-Game/src/login.js
+++ Remote Data and Authentication/Exercises/05.Fisher-Game/src/login.js
@@ -1,1 +1,43 @@
-console.log('TODO:// Implement Login functionality');
+window.addEventListener('DOMContentLoaded',()=>{
+ const form = document.querySelector('form');
+ form.addEventListener('submit',onLogin);
+})
+
+async function onLogin(event){
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+    
+
+    try {
+        const res = await fetch('http://localhost:3030/users/login',{
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({email,password})
+        });
+        console.log(res)
+        if (res.ok != true) {
+            const error =await res.json();
+            throw new Error(error.message);
+        }
+        
+        const data = await res.json();
+
+        console.log(data)
+
+        const userData = {
+            email: data.email,
+            id: data._id,
+            token: data.accessToken,
+        };
+
+        sessionStorage.setItem('userData', JSON.stringify(userData));
+
+       window.location='/';
+        
+    } catch (error) {
+        alert(error.message);
+    }
+}
