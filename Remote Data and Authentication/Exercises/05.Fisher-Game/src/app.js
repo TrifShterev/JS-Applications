cf889e50c4eb1eec6830f4bffd8d177184389cc9
Trifon Shterev <67158514+TrifShterev@users.noreply.github.com>
2022-02-18 14:10:46 +0200

    FISHER-Game task Completed

--- Remote Data and Authentication/Exercises/05.Fisher-Game/src/app.js
+++ Remote Data and Authentication/Exercises/05.Fisher-Game/src/app.js
@@ -2,22 +2,113 @@ let userData = null;
 //TODO: UPDATE, Delete and Logout buttons 
 window.addEventListener('DOMContentLoaded',()=>{
   userData = JSON.parse(sessionStorage.getItem('userData'));
-
+const logoutBtn =  document.getElementById('logout');
    if (userData != null) {
 
     document.getElementById('guest').style.display = 'none';
     document.querySelector('#addForm .add').disabled = false;
+    document.querySelector('.email > span').innerHTML = userData.email;
        
    } else {
     document.getElementById('user').style.display = 'none';
-       
+       logoutBtn.style.display = '';
    }
 
    document.querySelector('.load').addEventListener('click',loadData);
-
+   
    document.getElementById('addForm').addEventListener('submit', onCreateSubmit);
+   
+   const catches = document.getElementById('catches');
+   catches.addEventListener('click',onTableClick);
+   
+  logoutBtn.addEventListener('click',onLogout);
 
    });
+
+   function onLogout() {
+       sessionStorage.clear();
+       window.location = '/index.html'
+
+   }
+
+    function onTableClick(event) {
+       
+    if (event.target.className == 'update') {
+
+        const id = event.target.dataset.id;
+       
+        onUpdate(event.target,id);
+
+    } else if (event.target.className == 'delete') {
+       
+        onDelete(event.target);
+    }
+}
+
+async function onUpdate(button,id){
+   
+const currentCatchDiv = button.parentNode;
+
+    const newAngler = currentCatchDiv.querySelector('.angler').value;
+    const newBait = currentCatchDiv.querySelector('.bait').value;
+    const newCaptureTime = currentCatchDiv.querySelector('.captureTime').value;
+    const newLocation = currentCatchDiv.querySelector('.location').value;
+    const newSpecies = currentCatchDiv.querySelector('.captureTime').value;
+    const newWeight = currentCatchDiv.querySelector('.weight').value;
+
+const updatedCatch = {
+    angler: newAngler,
+    bait: newBait,
+    captureTime: newCaptureTime,
+    location: newLocation,
+    species: newSpecies,
+    weight: newWeight
+}
+   
+   
+     
+     try {
+          if (Object.values(updatedCatch).some(value => value == '')) {
+              throw new Error('All fields are required');
+          }
+         
+         await updateCatch(id,updatedCatch);      
+
+          loadData();
+        }catch (err) {
+            alert(err.message);
+        }
+}
+
+async function updateCatch(id, huntCatch) {
+    const url = 'http://localhost:3030/data/catches/' + id;
+
+    const updatedCatch = await fetch(url, {
+        method: 'PUT',
+        headers: {'X-Authorization': userData.token},
+        body: JSON.stringify(huntCatch)
+    });
+    return updatedCatch;
+}
+
+   async function onDelete(button) {
+       const id = button.dataset.id;
+       await deleteMethod(id);
+       button.parentNode.remove();
+   }
+
+   async function deleteMethod(id){
+
+    const response = await fetch('http://localhost:3030/data/catches/'+id,{
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json',
+        'X-Authorization' : userData.token}
+    });
+    
+    const result = await response.json();
+   
+    return result;
+   }
 
    async function onCreateSubmit(event){
        event.preventDefault();
@@ -57,12 +148,12 @@ window.addEventListener('DOMContentLoaded',()=>{
 
    }
 
-  async function loadData(){
+  async function loadData(event){
 
     const response = await fetch('http://localhost:3030/data/catches');
     const data = await response.json();
 
-    document.getElementById('catches').replaceChildren(...data.map(createPreview))
+    catches.replaceChildren(...data.map(createPreview))
     
    };
 
